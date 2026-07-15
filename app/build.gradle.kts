eb43662a8bef15a718e9542d7d40df6479acd551
Seongwuk Park <seongwuk.park@gmail.com>
2026-07-15 19:56:06 +1200

    chore: 버전을 0.5로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         applicationId = "org.goodexpert.ridefit"
         minSdk = 28
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 5
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
